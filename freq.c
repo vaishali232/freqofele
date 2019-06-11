04b1ba970167ca83138e8da4abcbdc22a90d6273
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 20:45:10 +0530

    Update freq.c

--- freq.c
+++ freq.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int n,flag=0,max=0,var;
+scanf("%d",&n);
+int a[n];
+int freq[n];
+for(int i=0;i<n;i++)
+{
+scanf("%d ",&a[i]);
+}
+for(int i=0;i<n;i++)
+{
+freq[i]=0;
+}
+for(int i=0;i<n;i++)
+{
+freq[a[i]]++;
+}
+ for(int i=0;i<n;i++)
+ {
+   if(freq[i]>max)
+   {
+     max=freq[i];
+     var=i;
+   }
+   
+ }
+  printf("%d",var);
 
+}
